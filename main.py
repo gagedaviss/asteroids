910e9f611945f9ce9d8c550d4342e4aedc69364b
gagedaviss <dgdavisgage60@gmail.com>
2025-11-20 08:36:27 -0600

    added start up to main and circleshape file

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
-version = pygame.version.ver
+
+
+
+
+
 
 def main():
+
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    version = pygame.version.ver
+    
     print(f"Starting Asteroids with pygame version: {version}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+ 
 
 
 
