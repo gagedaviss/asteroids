1e9c85e3104b5587e9ea43e5a6a113f0761893ab
gagedaviss <dgdavisgage60@gmail.com>
2025-11-21 10:14:37 -0600

    added player to screen

--- constants.py
+++ constants.py
@@ -1,2 +1,8 @@
+#Size of the screen 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+#Size of the player
+PLAYER_RADIUS = 20
+LINE_WIDTH = 2 
+
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
-
-
-
-
+from player import Player
 
 
 def main():
@@ -19,17 +15,25 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000.0
 
-
-
- 
+        
+        #print(dt)
 
 
 
